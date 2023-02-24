ec16199cfef95c641ea2b11be26976dc08f4d4ac
hassan <hassanabud03@gmail.com>
2023-02-24 15:39:22 +0300

    changed the fish prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function fish_prompt
+    set -l prompt_symbol '$'
+    fish_is_root_user; and set prompt_symbol '#'
+
+    echo -s (set_color blue) (prompt_pwd) \
+     " "\
+    (set_color yellow) $prompt_symbol (set_color normal) \
+    " "
+end
+
 fish_add_path /home/hassan/repos/Odin
 fish_add_path /home/hassan/.local/bin
 
